a95784063a5f24c0b46d2b0f24c7c19d82bb28db
Jappkuwu <loyrxclause@gmail.com>
2020-04-29 09:56:56 +0200

    29.04.2020

---
+++ Assets/Scripts/GolfClub.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityAtoms.Mobile;
+using UnityEngine;
+
+public class GolfClub : MonoBehaviour
+
+{
+    public FloatReference hitpower;
+    public Vector2Variable hitValue;
+    public Rigidbody2D ballRb2D;
+
+
+
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void ProcessDrag(TouchUserInput tui)
+    {
+
+        if (tui.InputState == TouchUserInput.State.Up)
+        {
+            Vector2 hitVector = tui.InputPos - tui.InputPosLastDown;
+            hitValue.Value = hitpower * hitVector;
+            ballRb2D.AddForce(hitValue.Value);
+        }
+    }
+}
---
+++ Assets/Scripts/TrajectoryPrediction.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+public class TrajectoryPrediction : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2Variable hitValue;
+
+    [SerializeField]
+    private float predictionTime = 2.0f;
+
+    [SerializeField]
+    private LineRenderer lineRenderer;
+
+    [SerializeField]
+    private Transform target;
+    private Rigidbody2D targetRb2D;
+
+    private List<Vector3> trajectoryPoints = new List<Vector3>();
+
+
+
+    private void Start()
+    {
+        targetRb2D = target.GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void Update()
+    {
+        
+    }
+
+    private void DisplayTrajectory()
+    {
+        int maxIterations = Mathf.RoundToInt(predictionTime / Time.fixedDeltaTime);
+            Vector2 pos = Vector2.zero;
+        Vector2 vel = targetRb2D.velocity + hitValue.Value / targetRb2D.mass;
+
+        float drag = targetRb2D.drag;
+
+        trajectoryPoints.Add(pos);
+
+        for (int i=0; i < maxIterations; i++)
+        {
+            vel += Physics2D.gravity * Time.fixedDeltaTime;
+            vel *= Mathf.Clamp01(1.0f - (drag * Time.fixedDeltaTime));
+        }
+    }    
+}
